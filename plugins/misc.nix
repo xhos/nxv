d088d9478424a2b790568de31dea228a36ce9ad5
xhos <60789741+xhos@users.noreply.github.com>
2025-10-30 21:16:27 -0400

    add neocord

--- plugins/misc.nix
+++ plugins/misc.nix
@@ -1,6 +1,12 @@
 {
   vim = {
     repl.conjure.enable = true;
+    presence.neocord = {
+      enable = true;
+      setupOpts = {
+        logo = "https://pics.xhos.dev/u/ns8jGz.png";
+      };
+    };
     mini.indentscope = {
       enable = true;
       setupOpts = {
