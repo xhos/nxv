a637147cac5e49039851a70c8c4e1e7d0227a898
xhos <60789741+xhos@users.noreply.github.com>
2025-12-25 01:46:05 -0500

    nice alpha dashboard

--- plugins/alpha.nix
+++ plugins/alpha.nix
@@ -1,48 +1,87 @@
-{
+{ lib, ... }: 
+let
+  # Define the button helper
+  mkButton = { command, shortcut, text }: {
+    type = "button";
+    val = text;
+    on_press = { 
+      __raw = ''
+        function()
+          local key = vim.api.nvim_replace_termcodes("${command}", true, false, true)
+          vim.api.nvim_feedkeys(key, "normal", false)
+        end
+      '';
+    };
+    opts = {
+      inherit shortcut;
+      keymap = [
+        "n"
+        shortcut
+        command
+        { noremap = true; silent = true; nowait = true; }
+      ];
+      position = "center";
+      hl = "DashboardCenter";
+      cursor = 3;
+      width = 35;
+      align_shortcut = "right";
+      spacing = 1;
+    };
+  };
+in {
   vim.dashboard.alpha = {
     enable = true;
-  };
-}
-#TODO: convert the rest
-# return {
-#   'goolord/alpha-nvim',
-#   config = function()
-#     local alpha = require('alpha')
-#     local dashboard = require('alpha.themes.dashboard')
-#     local function get_random_header()
-#       local ascii_dir = vim.fn.stdpath('config') .. '/ascii'
-#       local files = vim.fn.glob(ascii_dir .. '/*.lua', 0, 1)
-#       if #files == 0 then
-#         return nil
-#       end
-#       local random_file = files[math.random(#files)]
-#       local success, header_data = pcall(dofile, random_file)
-#       if success and header_data then
-#         return header_data
-#       else
-#         return nil
-#       end
-#     end
-#     local header = get_random_header()
-#     if header then
-#       dashboard.section.header = header
-#     else
-#       dashboard.section.header.val = { "No ASCII art found" }
-#     end
-#     dashboard.section.buttons.val = {
-#       dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-#       dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
-#       dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
-#       dashboard.button("s", "  Settings", ":e $MYVIMRC<CR>"),
-#       dashboard.button("q", "  Quit", ":qa<CR>"),
-#     }
-#     dashboard.config.layout = {
-#       dashboard.section.header,
-#       { type = "padding", val = 2 },
-#       dashboard.section.buttons,
-#     }
-#     alpha.setup(dashboard.config)
-#   end,
-#   dependencies = { 'nvim-tree/nvim-web-devicons' }
-# }
+    theme = null;
+    
+    layout = [
+      {
+        type = "text";
+        val = [
+          "                                              "
+          "      ████ ██████           █████      ██"
+          "     ███████████             █████ "
+          "     █████████ ███████████████████ ███   ███████████"
+          "    █████████  ███    █████████████ █████ ██████████████"
+          "   █████████ ██████████ █████████ █████ █████ ████ █████"
+          " ███████████ ███    ███ █████████ █████ █████ ████ █████"
+          "██████  █████████████████████ ████ █████ █████ ████ ██████"
+        ];
+        opts = {
+          position = "center";
+          hl = "Type";
+        };
+      }
 
+      { type = "padding"; val = 2; }
+
+      {
+        type = "group";
+        opts = { spacing = 1; };
+        val = map mkButton [
+          {
+            text = "new";
+            shortcut = "e";
+            command = "<CMD>ene <BAR> startinsert<CR>";
+          }
+          {
+            text = "find";
+            shortcut = "f";
+            command = "<CMD>Telescope find_files<CR>";
+          }
+          {
+            text = "recent";
+            shortcut = "r";
+            command = "<CMD>Telescope oldfiles<CR>";
+          }
+          {
+            text = "quit";
+            shortcut = "q";
+            command = "<CMD>qa<CR>";
+          }
+        ];
+      }
+    ];
+  };
+  
+  vim.visuals.nvim-web-devicons.enable = true;
+}
