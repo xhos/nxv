fc0c8e871e4086eb60918ad2e5c78af24b8cc77d
xhos <60789741+xhos@users.noreply.github.com>
2026-04-02 21:44:46 -0400

    make nxv work on any architecture

--- flake.nix
+++ flake.nix
@@ -9,16 +9,24 @@
     nixpkgs,
     nvf,
   }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
   in {
-    packages.${system}.default =
-      (nvf.lib.neovimConfiguration {
+    packages = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = (nvf.lib.neovimConfiguration {
         inherit pkgs;
         modules = [
           ./settings
           ./plugins
         ];
       }).neovim;
+    });
   };
 }
